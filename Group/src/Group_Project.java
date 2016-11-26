2590e367a56400a0ea8fcb7cb90f5cb00b844c75
jessicacenami <cenamij@student.swosu.edu>
2016-11-25 20:58:14 -0600

    added code

---
+++ Group/src/Group_Project.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Group_Project {
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter your name");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		
+		Scanner scanner = new Scanner(System.in);
+		String Name=scanner.nextLine();
+		System.out.println("Hello" + Name );
+		
+
+	}
+
+}
